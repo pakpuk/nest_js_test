ae23af662a5932c43c1ecf08eea973bcc6751ef5
Ghaith <ghayth.1900@gmail.com>
2026-04-19 21:12:27 +0200

    workin

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './controller/users/users.controller';
 
-@Module({})
+@Module({
+  controllers: [UsersController]
+})
 export class UsersModule {}
