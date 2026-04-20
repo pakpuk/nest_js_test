bdf6290e522b40f2cecdc1e88f95f79be52fdd27
Ghaith <ghayth.1900@gmail.com>
2026-04-20 15:01:30 +0200

    create some end points

--- src/users/controller/users/users.controller.ts
+++ src/users/controller/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get()
+    getUsers(){
+        return {message: 'ghaith',email :'ghaith@example.com'}
+    }
+    @Get('fetch')
+    fetchUsers(){
+        return {message: 'Fetching users...'}
+    }
+}
