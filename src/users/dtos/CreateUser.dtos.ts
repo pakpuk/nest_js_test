28d953187992eece65d6b9224c4908183303a4ff
Ghaith <ghayth.1900@gmail.com>
2026-04-21 18:14:46 +0200

    workin in services

--- src/users/dtos/CreateUser.dtos.ts
+++ src/users/dtos/CreateUser.dtos.ts
@@ -1,4 +1,4 @@
 export class CreateUserDto {
-    username!: string;
+    userName!: string;
     email! : string; 
 }
--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dtos';
 
 @Injectable()
 export class UsersService {
-
+  private fakeUsers =[{userName: 'ghaith',email :'ghaith@example.com'},{userName: 'john',email :'john@example.com'}]
     fetchUsers(){
-        return [{message: 'ghaith',email :'ghaith@example.com'}]
+        return this.fakeUsers;
+    }
+
+    CreateUser( userDetails : CreateUserDto){
+        this.fakeUsers.push(userDetails);
     }
 }
