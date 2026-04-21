3fe3e917ee3743af16be513aed6cba6832f25aab
Ghaith <ghayth.1900@gmail.com>
2026-04-21 21:06:08 +0200

    create pipes

--- src/users/pipes/agepipe/agepipe.pipe.ts
+++ src/users/pipes/agepipe/agepipe.pipe.ts
@@ -1,8 +1,16 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dtos';
 
 @Injectable()
 export class AgepipePipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
+  transform(value: CreateUserDto, metadata: ArgumentMetadata) {
+console.log('Inside ValidateCreateUserPipe !)');
+console.log(value);
+console.log(metadata);
+
+const parseAgeToInt = parseInt(value.age.toString());
+console.log('${value.age} is not a number');
+ 
     return value;
   }
 }
